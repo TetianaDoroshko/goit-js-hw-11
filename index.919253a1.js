5e42eec6a44a5639693a4954a3e4ebbfebb990e5
TetianaDoroshko <95139041+TetianaDoroshko@users.noreply.github.com>
2022-06-18 11:05:37 +0000

    Deploying to gh-pages from @ TetianaDoroshko/goit-js-hw-11@cd2ba6dc17d9b6502af62b2cf00b539367881c00 🚀

--- index.919253a1.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector("#search-form"),document.querySelector(".search-form__input"),document.querySelector(".search-form__btn")][0].addEventListener("submit",(function(e){e.preventDefault()}));
-//# sourceMappingURL=index.919253a1.js.map
--- index.d6892e30.js
+++
@@ -1,2 +0,0 @@
-({form:document.querySelector("#search-form"),input:document.querySelector(".search-form__input"),btnSearch:document.querySelector(".search-form__btn")}).form.addEventListener("submit",(function(e){e.preventDefault()}));
-//# sourceMappingURL=index.d6892e30.js.map
